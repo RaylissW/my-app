37526bf680ed1e9b82ebea4687a5c1c380dedce4
Lissa <A.Mosh15@yandex.ru>
2019-12-16 15:24:40 +0500

    Кнопка добавить высвечивается только в момент редактирования пустой формы

--- src/Components/MakeAppBar.js
+++ src/Components/MakeAppBar.js
@@ -3,7 +3,6 @@ import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import {useStyles} from './useStyles';
 
 
@@ -14,16 +13,23 @@ export default function MakeAppBar(props) {
         <div className={classes.root}>
                 <AppBar position="fixed" className={classes.appBar}>
                     <Toolbar>
-                        <Typography>Список Сотрудников</Typography>
-                        <Button  className={classes.commandButton} className="Add" onClick={()=>props.addPerson()} color="inherit">
-                            Добавить
-                        </Button>
                         {(props.selectedIndex == undefined) ?
-                            <Button disabled id="test" className={classes.commandButton} className="Delete"
+                            <Button className={classes.commandButton} className="Add" onClick={() => props.addPerson()}
+                                    color="inherit">
+                                Добавить
+                            </Button>
+                            :
+                            <Button disabled className={classes.commandButton} className="Add" onClick={() => props.addPerson()}
+                                    color="inherit">
+                                Добавить
+                            </Button>
+                        }
+                        {(props.selectedIndex == undefined) ?
+                            <Button disabled  className={classes.commandButton} className="Delete"
                                     color="inherit" onClick={() => props.deletePerson()}>
                                 Удалить
                             </Button>
-                            : <Button id="test" className={classes.commandButton} className="Delete" color="inherit"
+                            : <Button  className={classes.commandButton} className="Delete" color="inherit"
                                       onClick={() => props.deletePerson()}>
                                 Удалить
                             </Button>
